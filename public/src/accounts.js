680032ed044ea2d345f4a3422d057102d0a4320e
Jack <riganjack40@gmail.com>
2021-02-01 20:42:32 -0700

    Updated accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -4,23 +4,28 @@ const findAccountById = (accounts,id) => accounts.find((account) => account.id =
 const sortAccountsByLastName = accounts => accounts.sort((user1, user2) => user1.name.last < user2.name.last ? -1 : 1)
 
   
-const numberOfBorrows = (accounts,books) => {
-  let result = books.reduce((acc, books) => {
-      for(let i = 0; i < books.borrows.length; i++) {
-          if(books.borrows[i].id === accounts.id) {
-              acc ++
-          }
-      }
-      return acc
-  }, 0)
-  return result
-}
+// const numberOfBorrows = (accounts,books) => {
+//   let result = books.reduce((acc, books) => {
+//       for(let i = 0; i < books.borrows.length; i++) {
+//           if(books.borrows[i].id === accounts.id) {
+//               acc ++
+//           }
+//       }
+//       return acc
+//   }, 0)
+//   return result
+// }
+
+// const numberOfBorrows = (accounts,books) => books.borrows.reduce((acc, books))
 
 // const numberOfBorrows = (accounts,books) => {
 //  return books.reduce((acc,book) => acc + book.borrows[0].id === accounts[0].id, 0 )
 // }
 
 // const booksBorrowedCount = books => books.reduce((acc, book) => acc + !book.borrows[0].returned, 0)
+
+const numberOfBorrows = books.reduce((acc, book) => acc += book.borrows.filter(borrow => borrow.id === account.id.length, 0))
+
 
 
 
@@ -40,3 +45,7 @@ module.exports = {
   numberOfBorrows,
   getBooksPossessedByAccount,
 };
+
+
+
+// git flow
